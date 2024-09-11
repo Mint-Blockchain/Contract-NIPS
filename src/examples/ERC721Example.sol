3594317e16c7880a67e6f4dd879444065f40195b
clark9248 <clark@mintchain.io>
2024-09-11 14:15:42 +0800

    del NIPSERC721

--- src/examples/ERC721Example.sol
+++ src/examples/ERC721Example.sol
@@ -4,16 +4,33 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "./NIPSERC721.sol";
 
-contract ERC721Example is Initializable, NIPSERC721 {
+contract ERC721Example is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+
+    uint256 private _nextTokenId;
+    string public _baseUri;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function _initInfo(bytes calldata extendData) internal override {
+    function initialize(address initialOwner, string memory name, string memory symbol, bytes calldata extendData) initializer public {
+        __ERC721_init(name, symbol);
+        __Ownable_init(initialOwner);
+        _initInfo(extendData);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseUri;
+    }
+
+    function mint(address to) external {
+        uint256 tokenId = ++_nextTokenId;
+        _safeMint(to, tokenId);
+    }
+
+    function _initInfo(bytes calldata extendData) internal {
         _baseUri = abi.decode(extendData, (string));
     }
 }
