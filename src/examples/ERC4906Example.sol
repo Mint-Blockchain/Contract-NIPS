c6780456a7d499118c0c8255de4fe6c7832b315f
zhaopengfei <zhaopengfei@192.168.1.8>
2024-09-18 09:25:25 +0800

    add ERC4906

---
+++ src/examples/ERC4906Example.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract ERC4906Example is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+
+    uint256 private _nextTokenId;
+    string public _baseUri;
+
+    event MetadataUpdate(uint256 _tokenId);
+
+    event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId);
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner, string memory name, string memory symbol, bytes calldata extendData) initializer public {
+        __ERC721_init(name, symbol);
+        __Ownable_init(initialOwner);
+        _initInfo(extendData);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseUri;
+    }
+
+    function mint(address to) external {
+        uint256 tokenId = ++_nextTokenId;
+        _safeMint(to, tokenId);
+    }
+
+    function _initInfo(bytes calldata extendData) internal {
+        _baseUri = abi.decode(extendData, (string));
+    }
+
+    function updateMetadata(uint256 tokenId) public onlyOwner {
+        // require(_exists(tokenId), "ERC4906: Update for nonexistent token");
+        emit MetadataUpdate(tokenId);  // Emit event for metadata update
+    }
+
+    function batchUpdateMetadata(uint256 fromTokenId, uint256 toTokenId) public onlyOwner {
+        require(toTokenId >= fromTokenId, "ERC4906: Invalid token ID range");
+        emit BatchMetadataUpdate(fromTokenId, toTokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Upgradeable) returns (bool) {
+        return interfaceId == bytes4(0x49064906) || super.supportsInterface(interfaceId);
+    }
+}
