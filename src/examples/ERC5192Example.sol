b03ec3e734ae294856a57a0a83d38d597b140570
NealCaffrey12 <13668265193@163.com>
2024-11-15 16:55:57 +0800

    fix: path

--- src/examples/ERC5192Example.sol
+++ src/examples/ERC5192Example.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../lib/erc5192/IERC5192.sol";
+import "./../lib/erc5192/IERC5192.sol";
 
 contract ERC5192Example is
     Initializable,
